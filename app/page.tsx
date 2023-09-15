48fa1978809a53d56e1024abb4de50b01a228c04
BakerAlblam <gallardo0@outlook.com>
2023-09-15 19:46:51 +0200

    tr

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
+        <h1 className='text-green-500'>bruh</h1>
       {data?.map((watches: Data) => (
           <div key={watches._id}>
-            <h1>Watch tite: {watches.title} </h1>
+            <h1 className='text-red-500'>Watch tite:  </h1>
           </div>
         ))}
           </div>
