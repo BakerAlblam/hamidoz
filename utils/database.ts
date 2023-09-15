a6e3fee645369f55e65dd49fe71eda041e634f71
BakerAlblam <gallardo0@outlook.com>
2023-09-15 19:50:52 +0200

    bomba

--- utils/database.ts
+++ utils/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || "");
+    await mongoose.connect("mongodb+srv://smsmgaming:Timarli123@cluster0.pmjmucw.mongodb.net/Cluster0?retryWrites=true&w=majority" || "");
     console.log("Connected to MongoDB.")
   } catch (error) {
     console.log(error);
