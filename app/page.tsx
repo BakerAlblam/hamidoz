1e7ec46be195504ac3971988e9acb56226059064
BakerAlblam <gallardo0@outlook.com>
2023-09-15 21:06:57 +0200

    blmbbbbbbb

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ interface Data {
 export default function Home() {
   const { data } = useQuery(["data"], async () => {
     const res = await axios.get("http://localhost:3000/api/watches")
+    console.log(res.data.watches);
     return res.data.watches
     
   })
@@ -24,7 +25,7 @@ export default function Home() {
         <h1 className='text-green-500'>bruh</h1>
       {data?.map((watches: Data) => (
           <div key={watches._id}>
-            <h1 className='text-red-500'>Watch tite:  </h1>
+            <h1 className='text-red-500'>Watch tite: {watches.title} </h1>
           </div>
         ))}
           </div>
