e696f446b386c0c319ab0e534c59bbedfd1cfbff
BakerAlblam <gallardo0@outlook.com>
2023-09-16 11:22:33 +0200

    get

--- app/api/watches/route.ts
+++ app/api/watches/route.ts
@@ -3,12 +3,6 @@ import connectMongoDB from "@/utils/database";
 import { NextResponse } from "next/server";
 
 
-export async function POST(request: Request) {
-    const {title} = await request.json()
-    await connectMongoDB()
-    await Watch.create({title})
-    return NextResponse.json({ message: "done"})
-}
 
 export async function GET() {
     await connectMongoDB();
